576795b435ed2ff7473436f259b777e4e4dc24cd
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-07-07 12:53:45 -0300

    Atualização do Script

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+// Ativação dos links do header
+const links = document.querySelectorAll(".header-menu a");
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+links.forEach(ativarLink);
+
+//Ativar itens orçamento
+const parametros = new URLSearchParams(location.search);
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
